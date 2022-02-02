a4984536815a37e8f7aaa25943d9e38b3caf4771
Joshua <yoonj200@gmail.com>
2022-02-02 16:58:12 -0600

    commit tree dae6a5aaa2ff1be8f5616adc68bc67315b8064c5

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+//uses default activity 'roller-skate' when no arguments are passed
+function saturdayFun(activity = `roller-skate`) { 
+    return `This Saturday, I want to ${activity}!`;
+} // permits the default activity to be overriden
+
+//uses the default activity 'go to the office' when no arguments are passed
+function mondayWork(activity = `go to the office`) {
+    return `This Monday, I will ${activity}.`;
+} // permits the default activity to be overriden
+
+//when initialized with '*' creates a function that, when called, wraps an adjective in a highlight
+//when initialized with '||' creates a function that, when called, wraps an adjective in a highlight
+
+function wrapAdjective(str=`*`) {
+    return function (name = `special`) {
+        return `You are ${str}${name}${str}!`
+    }
+}
